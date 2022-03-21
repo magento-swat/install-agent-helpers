a8ada59948f0239663bb7becd2a3850d84a91cd1
Alex Bomko <abomko@adobe.com>
2022-03-21 10:36:24 -0500

    Revert "Revert "SWAT-307: SWAT agent cannot be installed as a cron if crontab is empty""
    This reverts commit eac178c3d7fa5c9bd494d8ace110fbb521da1dd0.

--- install.sh
+++ install.sh
@@ -101,8 +101,9 @@ installAndConfigureCron() {
   local agentCommand="* * * * * flock -n /tmp/swat-agent.lockfile -c '$agentPath/scheduler' >> $agentPath/errors.log 2>&1"
   if [[ ! ("$cronContent" =~ "swat-agent") ]]; then
      (crontab -l; echo "$agentCommand") | crontab -
+     printSuccess "Please configure cron to run the agent using the following command:"
+     printSuccess "(crontab -l; echo \"$agentCommand\") | crontab -"
   fi
-  printSuccess "The cronjob has been configured. Review your cronjobs with the following command crontab -l"
 }
 
 installAndConfigureDaemon() {
