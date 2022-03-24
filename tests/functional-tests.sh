9563a97e752473dbd6b6dbaffad0b98828f91bcb
Yarik Voronyi <yvoronoy@adobe.com>
2022-03-23 18:43:13 -0700

    Small functional test with expect

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -2,5 +2,5 @@
 set -Eeuo pipefail
 
 docker build -f tests/Dockerfile -t installer-functional-test .
-docker run -v $(pwd):/app --rm -it installer-functional-test expect -f /app/tests/test-as-root-production
-docker run -v $(pwd):/app --rm -it installer-functional-test expect -f /app/tests/test-as-root-sandbox
+docker run -v $(pwd):/app --rm -i installer-functional-test expect -f /app/tests/test-as-root-production
+docker run -v $(pwd):/app --rm -i installer-functional-test expect -f /app/tests/test-as-root-sandbox
