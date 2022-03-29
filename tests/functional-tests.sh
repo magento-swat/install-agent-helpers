94b2f1e83ca1d6e236f18650d3fc782217a36581
Yarik Voronyi <yvoronoy@adobe.com>
2022-03-29 12:09:24 -0700

    Add negative test

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -4,3 +4,4 @@ set -Eeuo pipefail
 docker build -f tests/Dockerfile -t installer-functional-test .
 docker run -e TERM=xterm -e AGENT_SKIP_FIRST_RUN=1 -v "$(pwd)":/app --rm -i installer-functional-test expect -f /app/tests/test-as-root-production
 docker run -e TERM=xterm -e AGENT_SKIP_FIRST_RUN=1 -v "$(pwd)":/app --rm -i installer-functional-test expect -f /app/tests/test-as-root-sandbox
+docker run -e TERM=xterm -v "$(pwd)":/app --rm -i installer-functional-test bash /app/tests/test-skip-first-run-error-catch
